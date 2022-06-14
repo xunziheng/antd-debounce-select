a3dc05ab35ddb9af5c2739aa16c11a6303d14674
xunziheng <xunzh_hb@mingya.com.cn>
2022-06-14 17:14:03 +0800

    demo page;

--- .umirc.ts
+++ .umirc.ts
@@ -6,6 +6,7 @@ export default defineConfig({
     'https://avatars.githubusercontent.com/u/35691153?s=400&u=a0173e2ecd400de3e7ad38cde996f4cbaf777b2b&v=4',
   logo: 'https://avatars.githubusercontent.com/u/35691153?s=400&u=a0173e2ecd400de3e7ad38cde996f4cbaf777b2b&v=4',
   outputPath: 'docs-dist',
+  publicPath: './',
   mode: 'site',
   // more config: https://d.umijs.org/config
   extraBabelPlugins: [
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 **Links**
 [HomePage](https://github.com/xunziheng/antd-debounce-select)
+[Demo](https://xunziheng.github.io/antd-debounce-select)
 <br />
 <img src="https://raw.githubusercontent.com/xunziheng/antd-debounce-select/main/src/DebounceSelect/demos/demo.gif" alt="Demo">
